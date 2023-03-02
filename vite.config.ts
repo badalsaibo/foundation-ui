ed94185038b9020f34fbf53d20cfc04670001807
Badal Saibo <badalsaibo@gmail.com>
2023-03-02 17:36:57 +0530

    feat: linaria

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import linaria from '@linaria/vite';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [linaria(), react()],
 })
